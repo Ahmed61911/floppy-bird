097994b892544110018aee159f1800995cab4718
BABA Ahmed <ahmed.baba.ab.96@gmail.com>
2026-02-03 12:57:28 +0000

    Playable version of the game

--- main.py
+++ main.py
@@ -10,24 +10,88 @@ def main():
     pygame.display.set_caption('Floppy Bird')
     clock = pygame.time.Clock()
     running = True
-    dt = 0
+    pygame.font.init()
 
+    death_screen = False
+    game_on = False
     speed = 1
+    score = 0
+    pipe_counted = False
     pipe_spacing = 350
     pipe_gap = 170
-
-    score = 0
     y_pos = 240
     x_pos = 80
 
+
+    my_font = pygame.font.SysFont('font/Pixeltype.ttf', 40)
 
     bg_scroll = 0
     bg_surf = pygame.image.load('images/Bg.png').convert()
     bg_tiles = math.ceil(width / bg_surf.get_width()) + 1
 
+    #Main menu rectangles
+    play_btn_rect = pygame.Rect(width / 2 - 100, height / 2 - 100, 200, 80)
+    quit_btn_rect = pygame.Rect(width / 2 - 100, height / 2 + 0, 200, 80)
+
+    play_btn_surface = pygame.transform.scale(pygame.image.load('images/Play.png').convert() , (play_btn_rect.width, play_btn_rect.height))
+    quit_btn_surface = pygame.transform.scale(pygame.image.load('images/Quit.png').convert() , (quit_btn_rect.width, quit_btn_rect.height))
+    play_btn_text = my_font.render(f"PLAY", False, (60, 60, 60))
+    quit_btn_text = my_font.render(f"Quit", False, (10, 10, 10))
+    death_menu_surface = pygame.transform.scale(pygame.image.load('images/Death_bg.png').convert() , (width - 300, height - 200))
+
+
+    def show_menu():
+
+        draw_bg()
+        ground.draw()
+        #play button
+        screen.blit(play_btn_surface, (play_btn_rect.centerx - play_btn_surface.get_width() / 2, play_btn_rect.centery - play_btn_surface.get_height() / 2))
+        screen.blit(play_btn_text, (play_btn_rect.centerx - play_btn_text.get_width() / 2, play_btn_rect.centery - play_btn_text.get_height() / 2))
+        #pygame.draw.rect(screen, "Blue", play_btn_rect , 2)
+
+        #Quit button
+        screen.blit(quit_btn_surface, (quit_btn_rect.centerx - quit_btn_surface.get_width() / 2, quit_btn_rect.centery - quit_btn_surface.get_height() / 2))
+        screen.blit(quit_btn_text, (quit_btn_rect.centerx - quit_btn_text.get_width() / 2, quit_btn_rect.centery - quit_btn_text.get_height() / 2))
+        #pygame.draw.rect(screen, "Blue", quit_btn_rect , 2)
+
+    def death_menu():
+        draw_bg()
+        player.draw()
+        pipe1.draw()
+        pipe2.draw()
+        pipe3.draw()
+        ground.draw()
+
+
+        #Draw menu BG
+        screen.blit(death_menu_surface, (150, 25))
+
+        #play button
+        screen.blit(play_btn_surface, (play_btn_rect.centerx - play_btn_surface.get_width() / 2, play_btn_rect.centery - play_btn_surface.get_height() / 2))
+        screen.blit(play_btn_text, (play_btn_rect.centerx - play_btn_text.get_width() / 2, play_btn_rect.centery - play_btn_text.get_height() / 2))
+        #pygame.draw.rect(screen, "Blue", play_btn_rect , 2)
+
+        #Quit button
+        screen.blit(quit_btn_surface, (quit_btn_rect.centerx - quit_btn_surface.get_width() / 2, quit_btn_rect.centery - quit_btn_surface.get_height() / 2))
+        screen.blit(quit_btn_text, (quit_btn_rect.centerx - quit_btn_text.get_width() / 2, quit_btn_rect.centery - quit_btn_text.get_height() / 2))
+        #pygame.draw.rect(screen, "Blue", quit_btn_rect , 2)
+
+        #show score
+        dead_surface = my_font.render(f"GAME OVER !", False, ("red"))
+        score_surface = my_font.render(f"Your High score: {score}", False, ("white"))
+        screen.blit(dead_surface, (width / 2 - dead_surface.get_width() / 2, 100))
+        screen.blit(score_surface, (width / 2 - score_surface.get_width() / 2, 150))
+
     def draw_bg():
         for i in range(0, bg_tiles):
             screen.blit(bg_surf, (bg_surf.get_width() * i + bg_scroll, 0))
+
+    def show_score(score):
+        score_surface = my_font.render(f"Score: {score}", False, ("white"))
+        screen.blit(score_surface, (20, 20))
+
+
+
 
     class Player:
         def __init__(self, player_x,player_y):
@@ -62,7 +126,7 @@ def main():
 
         def draw(self):
             screen.blit(self.surface, (self.rect.x, self.rect.y))
-            pygame.draw.rect(screen, 'green', self.rect, 1)
+            #pygame.draw.rect(screen, 'green', self.rect, 1)
 
         def collision(self):
             return (
@@ -91,10 +155,9 @@ def main():
         def draw(self):
             for i in range(0, self.tiles):
                 screen.blit(self.surface, (self.surface.get_width() * i - i + self.scroll, bg_surf.get_height() - 10))
-            pygame.draw.rect(screen, 'yellow', self.rect, 1)
+            #pygame.draw.rect(screen, 'yellow', self.rect, 1)
             if abs(self.scroll)  > self.surface.get_width():
                 self.scroll = 0
-
 
 
     class Pipe:
@@ -102,6 +165,7 @@ def main():
             self.off_screen = False
             self.pipe_w = 80
             self.pipe_h = 800
+            self.counted = False
 
             self.pipe_x = pipe_x_pos
 
@@ -119,17 +183,20 @@ def main():
 
 
         def draw(self):
-            self.pipe_x-= speed
+            if game_on:
+                self.pipe_x-= speed
 
-            self.rect1.y = self.pipe1_y + 30
-            self.rect1.x = self.pipe_x + 35
-            self.rect2.x = self.pipe_x + 35
-            self.rect2.y = self.pipe2_y - 30
+                self.rect1.y = self.pipe1_y + 30
+                self.rect1.x = self.pipe_x + 35
+                self.rect2.x = self.pipe_x + 35
+                self.rect2.y = self.pipe2_y - 30
 
-            screen.blit(self.surface1, (self.pipe_x, self.pipe1_y))
-            screen.blit(self.surface2, (self.pipe_x, self.pipe2_y))
-            pygame.draw.rect(screen, "Blue", self.rect1, 1)
-            pygame.draw.rect(screen, "Blue", self.rect2, 1)
+                screen.blit(self.surface1, (self.pipe_x, self.pipe1_y))
+                screen.blit(self.surface2, (self.pipe_x, self.pipe2_y))
+                #pygame.draw.rect(screen, "Blue", self.rect1, 1)
+                #pygame.draw.rect(screen, "Blue", self.rect2, 1)
+            else:
+                pass
                 
 
         
@@ -151,47 +218,83 @@ def main():
                 running = False
                 exit()
 
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    if play_btn_rect.collidepoint(event.pos):
+                        game_on = True
+                        y_pos = 240
+                        x_pos = 80
+                        
+
+                    elif quit_btn_rect.collidepoint(event.pos):
+                        running = False
+                        exit()
+
+
+
         #fill frame
         screen.fill("black")
 
+        #If the game has started
+        if game_on :
             #update objects
-        draw_bg()
+                #set speed
+            speed = min(speed + 1/500, 6)
+            bg_scroll -= 0.5 * speed
+            ground.scroll -= 1 * speed
 
-        speed = min(speed + 1/500, 6)
-        bg_scroll -= 0.5 * speed
-        ground.scroll -= 1 * speed
-        if abs(bg_scroll) > bg_surf.get_width():
-            bg_scroll = 0
+                #Repeat Background
+            if abs(bg_scroll) > bg_surf.get_width():
+                bg_scroll = 0
 
-        pipes = [pipe1, pipe2, pipe3]
+                #Set pipe positions
+            pipes = [pipe1, pipe2, pipe3]
 
-        for pipe in pipes:
-            if pipe.pipe_x < -150:
-                pipe.pipe_x = max(p.pipe_x for p in pipes) + pipe_spacing
-                pipe.pipe1_y = random.randint(150, 350)
-                pipe.pipe2_y = pipe.pipe1_y - pipe_gap - pipe.surface2.get_height()
-                if player.rect.x + player.rect.width > pipe.rect1.x :
+            for pipe in pipes:
+                    #Updating the postion of the pipes
+                if pipe.pipe_x < -150:
+                    pipe.pipe_x = max(p.pipe_x for p in pipes) + pipe_spacing
+                    pipe.pipe1_y = random.randint(150, 350)
+                    pipe.pipe2_y = pipe.pipe1_y - pipe_gap - pipe.surface2.get_height()
+                    pipe_counted = False
+                    #score systeme
+                elif pipe.pipe_x < player.player_x and pipe_counted == False:
                     score += 1
+                    pipe_counted = True
 
-        print(score)
+                #Check movement and collsion
+            player.jump(input)
+            if player.collision():
+                game_on = False
+                death_screen = True
+                death_menu()
+                player = Player(x_pos, y_pos)
+                ground = Ground()
+                pipe1 = Pipe(width + 20)
+                pipe2 = Pipe(pipe1.pipe_x + random.randint(400, width))
+                pipe3 = Pipe(pipe2.pipe_x + random.randint(400, width))
 
-        player.jump(input)
-        if player.collision():
-            print("Collision")
-            #show objects
-        player.draw()
-        pipe1.draw()
-        pipe2.draw()
-        pipe3.draw()
-        ground.draw()
+                #show objects
+            draw_bg()
+            player.draw()
+            pipe1.draw()
+            pipe2.draw()
+            pipe3.draw()
+            ground.draw()
+            show_score(score)
+        
+        elif game_on == False and death_screen == False:
+            show_menu()
 
-
+        elif death_screen:
+            game_on = False
+            death_menu()
 
         #update frame
         pygame.display.update()
 
         #Frame limiter
-        dt = clock.tick(60) / 1000
+        clock.tick(60) / 1000
 
 main()
 
